7545dd587e17ed99ade4e39c1b6e2eb7a0e9eb66
Dmytro <ihnatenko.dev@gmail.com>
2022-11-06 21:06:24 +0200

    Create model User

---
+++ server/src/models/User.ts
@@ -0,0 +1,14 @@
+import { Schema, model, Types } from 'mongoose';
+
+const userSchema = new Schema({
+	email: { type: String, required: true, unique: true },
+	password: { type: String, required: true },
+	diskSpace: { type: Number, default: 1024 ** 3 * 10 },
+	usedSpace: { type: Number, default: 0 },
+	avatar: { type: String },
+	files: [{ type: Types.ObjectId, ref: 'File' }],
+});
+
+const User = model('User', userSchema);
+
+export default User;
