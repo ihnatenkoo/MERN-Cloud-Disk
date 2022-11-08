9deb15603e713e27d20912d9b1a71d139b0c209f
Dmytro <ihnatenko.dev@gmail.com>
2022-11-08 16:14:33 +0200

    Add validation: POST /api/auth/register

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -1,28 +1,46 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
+import { check, validationResult } from 'express-validator';
 import bcrypt from 'bcryptjs';
 import User from '../models/User';
 
 export const authRouter: Router = Router();
 
-authRouter.post('/register', async (req, res) => {
-	try {
-		const { email, password, avatar } = req.body;
+authRouter.post(
+	'/register',
+	[
+		check('email', 'Incorrect email').isEmail(),
+		check(
+			'password',
+			'Password must be longer than 3 and shorter than 12 symbols'
+		).isLength({ min: 3, max: 12 }),
+	],
+	async (req: Request, res: Response) => {
+		const errors = validationResult(req);
+		console.log(errors);
 
-		const isUserExist = await User.find({ email });
-
-		if (isUserExist.length) {
-			return res
-				.status(400)
-				.json({ message: `User with email ${email} already exist` });
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ message: `Incorrect request` });
 		}
 
-		const hashPassword = await bcrypt.hash(password, 15);
-		const user = new User({ email, password: hashPassword, avatar });
-		await user.save();
+		try {
+			const { email, password, avatar } = req.body;
 
-		return res.json({ message: 'User was created' });
-	} catch (error) {
-		console.log(error);
-		res.send({ message: 'Server Error' });
+			const isUserExist = await User.find({ email });
+
+			if (isUserExist.length) {
+				return res
+					.status(400)
+					.json({ message: `User with email ${email} already exist` });
+			}
+
+			const hashPassword = await bcrypt.hash(password, 15);
+			const user = new User({ email, password: hashPassword, avatar });
+			await user.save();
+
+			return res.json({ message: 'User was created' });
+		} catch (error) {
+			console.log(error);
+			res.send({ message: 'Server Error' });
+		}
 	}
-});
+);
