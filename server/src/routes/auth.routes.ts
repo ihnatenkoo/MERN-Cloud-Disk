04c2321937e5ebf5980a5bbd9cb44dfc9c6db2e2
Dmytro <ihnatenko.dev@gmail.com>
2022-11-09 20:02:19 +0200

    Server: validate auth POST /login

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -10,6 +10,7 @@ export const authRouter: Router = Router();
 authRouter.post(
 	'/register',
 	[
+		check('name', 'Too short name').isLength({ min: 3 }),
 		check('email', 'Incorrect email').isEmail(),
 		check(
 			'password',
@@ -46,37 +47,56 @@ authRouter.post(
 	}
 );
 
-authRouter.post('/login', async (req, res) => {
-	try {
-		const { email, password } = req.body;
-		const user = await User.findOne({ email });
+authRouter.post(
+	'/login',
+	[
+		check('email', 'Incorrect email').isEmail(),
+		check(
+			'password',
+			'Password must be longer than 3 and shorter than 12 symbols'
+		).isLength({ min: 3, max: 12 }),
+	],
+	async (req: Request, res: Response) => {
+		try {
+			const errors = validationResult(req);
 
-		if (!user) {
-			return res.status(404).json({ message: 'User not found' });
+			if (!errors.isEmpty()) {
+				return res.status(401).json({ message: `Incorrect login data` });
+			}
+
+			const { email, password } = req.body;
+			const user = await User.findOne({ email });
+
+			if (!user) {
+				return res
+					.status(404)
+					.json({ message: `User with email ${email} not found` });
+			}
+
+			const isPassValid = bcrypt.compareSync(password, user.password);
+
+			if (!isPassValid) {
+				return res.status(401).json({ message: 'Invalid password' });
+			}
+
+			const token = jwt.sign({ id: user.id }, config.get('secret-key'), {
+				expiresIn: '1h',
+			});
+
+			res.json({
+				token,
+				user: {
+					name: user.name,
+					id: user.id,
+					email: user.email,
+					diskSpace: user.diskSpace,
+					usedSpace: user.usedSpace,
+					avatar: user.avatar,
+				},
+			});
+		} catch (error) {
+			console.log(error);
+			res.send({ message: 'Server Error' });
 		}
-
-		const isPassValid = bcrypt.compareSync(password, user.password);
-
-		if (!isPassValid) {
-			return res.status(401).json({ message: 'Invalid password' });
-		}
-
-		const token = jwt.sign({ id: user.id }, config.get('secret-key'), {
-			expiresIn: '1h',
-		});
-
-		res.json({
-			token,
-			user: {
-				id: user.id,
-				email: user.email,
-				diskSpace: user.diskSpace,
-				usedSpace: user.usedSpace,
-				avatar: user.avatar,
-			},
-		});
-	} catch (error) {
-		console.log(error);
-		res.send({ message: 'Server Error' });
 	}
-});
+);
