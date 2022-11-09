5aa78ea789b40793f7e141fba6bb6e792d8f645b
Dmytro <ihnatenko.dev@gmail.com>
2022-11-09 17:02:44 +0200

    Client: fix <RegisterForm/>, add state and submit func

--- client/src/components/forms/RegisterForm/RegisterForm.tsx
+++ client/src/components/forms/RegisterForm/RegisterForm.tsx
@@ -1,13 +1,54 @@
-import { FC } from 'react';
+import axios from 'axios';
+import { ChangeEvent, FC, useState } from 'react';
 import FormBtn from '../../ui/FormBtn/FormBtn';
 import s from './RegisterForm.module.scss';
 
 const RegisterForm: FC = () => {
+	const [data, setData] = useState({
+		name: '',
+		email: '',
+		password: '',
+	});
+
+	const onRegisterDataSubmit = async (): Promise<void> => {
+		try {
+			const req = await axios.post(import.meta.env.VITE_URL, {
+				name: data.name,
+				email: data.email,
+				password: data.password,
+			});
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	const onChangeValue = (e: ChangeEvent<HTMLInputElement>): void => {
+		setData({ ...data, [e.target.name]: e.target.value });
+	};
+
 	return (
-		<form className={s.form}>
-			<input type="text" placeholder="Enter name" />
-			<input type="text" placeholder="Enter email" />
-			<input type="text" placeholder="Enter password" />
+		<form className={s.form} onSubmit={onRegisterDataSubmit}>
+			<input
+				type="text"
+				value={data.name}
+				name="name"
+				placeholder="Enter name"
+				onChange={onChangeValue}
+			/>
+			<input
+				type="text"
+				value={data.email}
+				name="email"
+				placeholder="Enter email"
+				onChange={onChangeValue}
+			/>
+			<input
+				type="password"
+				value={data.password}
+				name="password"
+				placeholder="Enter password"
+				onChange={onChangeValue}
+			/>
 
 			<FormBtn className={s.btn}>Register</FormBtn>
 		</form>
