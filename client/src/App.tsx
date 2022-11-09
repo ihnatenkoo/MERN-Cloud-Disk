a3690a3e9e611e842f054289db15610943fa16d4
Dmytro <ihnatenko.dev@gmail.com>
2022-11-09 15:18:33 +0200

    Create <Header/>, add routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,20 @@
-const App = () => {
-	return <h1>Hello App</h1>;
+import { FC } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/Header/Header';
+
+const App: FC = () => {
+	return (
+		<BrowserRouter>
+			<Header />
+			<main>
+				<Routes>
+					<Route path="/" element={<h1>Main</h1>} />
+					<Route path="/register" element={<h1>Register</h1>} />
+					<Route path="/login" element={<h1>Login</h1>} />
+				</Routes>
+			</main>
+		</BrowserRouter>
+	);
 };
 
 export default App;
