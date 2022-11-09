b0a762f501e8f52b2bf6b26c8d0d5f801a669be6
Dmytro <ihnatenko.dev@gmail.com>
2022-11-09 16:37:40 +0200

    Create <LoginPage/>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/Header/Header';
+import LoginPage from './pages/LoginPage/LoginPage';
 import RegisterPage from './pages/RegisterPage/RegisterPage';
 
 const App: FC = () => {
@@ -11,7 +12,7 @@ const App: FC = () => {
 				<Routes>
 					<Route path="/" element={<h1>Main</h1>} />
 					<Route path="/register" element={<RegisterPage />} />
-					<Route path="/login" element={<h1>Login</h1>} />
+					<Route path="/login" element={<LoginPage />} />
 				</Routes>
 			</main>
 		</BrowserRouter>
