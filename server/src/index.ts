fdd647dd25098954a346e0ec2023ce2b6f296f13
Dmytro <ihnatenko.dev@gmail.com>
2022-11-09 19:19:05 +0200

    Server: install and add cors middleware

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import config from 'config';
+import cors from 'cors';
 import { authRouter } from './routes/auth.routes';
 
 const app = express();
 const PORT = config.get('serverPort');
 
+app.use(cors());
 app.use(express.json());
 app.use('/api/auth', authRouter);
 
