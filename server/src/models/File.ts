dd385b409b95b1fec1dabffc4031643e979894ce
Dmytro <ihnatenko.dev@gmail.com>
2022-11-11 17:43:06 +0200

    Server: create File model

---
+++ server/src/models/File.ts
@@ -0,0 +1,16 @@
+import { Schema, model, Types } from 'mongoose';
+
+const fileSchema = new Schema({
+	name: { type: String, required: true },
+	type: { type: String, required: true },
+	accessLink: { type: String },
+	size: { type: Number, default: 0 },
+	path: { type: String, default: '' },
+	user: { type: Types.ObjectId, ref: 'User' },
+	parent: { type: Types.ObjectId, ref: 'File' },
+	children: { type: Types.ObjectId, ref: 'File' },
+});
+
+const File = model('File', fileSchema);
+
+export default File;
