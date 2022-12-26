c8e558cbdc66c7b960ee577fa26608500abcb133
Dmytro <ihnatenko.dev@gmail.com>
2022-12-26 18:06:03 +0200

    Client: impl drag-n-drop area

--- client/src/pages/MainPage/MainPage.tsx
+++ client/src/pages/MainPage/MainPage.tsx
@@ -1,13 +1,56 @@
-import { FC, useState } from 'react';
+import { DragEvent, FC, useState } from 'react';
 import CreateDirPopup from '../../components/CreateDirPopup/CreateDirPopup';
 import FileList from '../../components/FileList/FileList';
 import FileNavigation from '../../components/FileNavigation/FileNavigation';
+import { useAppDispatch, useAppSelector } from '../../hooks';
+import { uploadFile } from '../../store/files/files.slice';
+import s from './MainPage.module.scss';
 
 const MainPage: FC = () => {
 	const [showPopup, setShowPopup] = useState<boolean>(false);
+	const [dragEnter, setDragEnter] = useState<boolean>(false);
+	const dispatch = useAppDispatch();
+	const dirId = useAppSelector((state) => state.files.currentDir);
 
-	return (
-		<section>
+	const dragEnterHandler = (event: DragEvent) => {
+		event.preventDefault();
+		event.stopPropagation();
+		setDragEnter(true);
+	};
+
+	const dragLeaveHandler = (event: DragEvent) => {
+		event.preventDefault();
+		event.stopPropagation();
+		setDragEnter(false);
+	};
+
+	const onDropHandler = (event: DragEvent) => {
+		event.preventDefault();
+		event.stopPropagation();
+		let files = [...event.dataTransfer.files];
+		files.forEach((file: string | Blob) => {
+			dispatch(uploadFile({ file, dirId }));
+		});
+		setDragEnter(false);
+	};
+
+	return dragEnter ? (
+		<div
+			className={s.drop_area}
+			onDragEnter={dragEnterHandler}
+			onDragLeave={dragLeaveHandler}
+			onDragOver={dragEnterHandler}
+			onDrop={onDropHandler}
+		>
+			Drag files here...
+		</div>
+	) : (
+		<section
+			className={s.main}
+			onDragEnter={dragEnterHandler}
+			onDragLeave={dragLeaveHandler}
+			onDragOver={dragEnterHandler}
+		>
 			<FileNavigation setShowPopup={setShowPopup} />
 			<FileList />
 			<CreateDirPopup showPopup={showPopup} setShowPopup={setShowPopup} />
